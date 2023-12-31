9f7b4746f18d62f8c7ebe900416b320e7fd9da3b
Abhijeet Rawat <abhijeet.rawat@zscaler.com>
2024-01-01 00:39:20 +0530

    Fix tmux restore

--- dot_config/nvim/lua/foureyez/plugins/harpoon.lua
+++ dot_config/nvim/lua/foureyez/plugins/harpoon.lua
@@ -1,30 +1,30 @@
 return {
-  "ThePrimeagen/harpoon",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-  },
-  lazy = true,
-  ft = { "go", "yaml" },
-  config = function()
-    require("harpoon").setup({})
+	"ThePrimeagen/harpoon",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+	},
+	lazy = true,
+	ft = { "go", "yaml", "lua", "toml", "json" },
+	config = function()
+		require("harpoon").setup({})
 
-    local wk = require("which-key")
-    wk.register({
-      ["<leader>h"] = {
-        name = "harpoon",
-        a = {
-          function()
-            require("harpoon.mark").add_file()
-          end,
-          "Mark File",
-        },
-        u = {
-          function()
-            require("harpoon.ui").toggle_quick_menu()
-          end,
-          "Toggle Quick menu",
-        },
-      },
-    })
-  end,
+		local wk = require("which-key")
+		wk.register({
+			["<leader>h"] = {
+				name = "harpoon",
+				a = {
+					function()
+						require("harpoon.mark").add_file()
+					end,
+					"Mark File",
+				},
+				u = {
+					function()
+						require("harpoon.ui").toggle_quick_menu()
+					end,
+					"Toggle Quick menu",
+				},
+			},
+		})
+	end,
 }
