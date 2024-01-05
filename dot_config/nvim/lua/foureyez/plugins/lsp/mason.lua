f8b843d9d52626ab1688d080d72f373be57062ee
Abhijeet Rawat <abhijeet.rawat@zscaler.com>
2024-01-06 01:29:05 +0530

    Cleanup

--- dot_config/nvim/lua/foureyez/plugins/lsp/mason.lua
+++ dot_config/nvim/lua/foureyez/plugins/lsp/mason.lua
@@ -1,18 +1,18 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    require("mason").setup({
-      ensure_installed = {
-        "gopls",
-        "pyright",
-        "lua-language-server",
-        "ruff",
-        "rust-analyzer",
-        "svelte-language-server",
-      },
-    })
-  end,
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
+	config = function()
+		require("mason").setup({
+			ensure_installed = {
+				"gopls",
+				"pyright",
+				"lua-language-server",
+				"ruff",
+				"rust-analyzer",
+				"svelte-language-server",
+			},
+		})
+	end,
 }
