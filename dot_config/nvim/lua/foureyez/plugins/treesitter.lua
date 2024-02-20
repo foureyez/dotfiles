728affc8084e64ae4014d43a64fea7ecea4b068e
Abhijeet Rawat <arawat@Abhijeets-MacBook-Air.local>
2024-02-21 00:04:35 +0530

    Add incremental selection

--- dot_config/nvim/lua/foureyez/plugins/treesitter.lua
+++ dot_config/nvim/lua/foureyez/plugins/treesitter.lua
@@ -34,19 +34,59 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
+          node_incremental = "v",
+          node_decremental = "v",
         },
       },
       textobjects = {
+        select = {
+          enable = true,
+
+          -- automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+
+          keymaps = {
+            -- you can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ib"] = "@block.inner",
+            ["ab"] = "@block.outer",
+            -- you can optionally set descriptions to the mappings (used in the desc parameter of
+            -- nvim_buf_set_keymap) which plugins like which-key display
+            ["ic"] = { query = "@class.inner", desc = "select inner part of a class region" },
+            -- you can also use captures from other query groups like `locals.scm`
+            ["as"] = { query = "@scope", query_group = "locals", desc = "select language scope" },
+          },
+          -- you can choose the select mode (default is charwise 'v')
+          --
+          -- can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * method: eg 'v' or 'o'
+          -- and should return the mode ('v', 'v', or '<c-v>') or a table
+          -- mapping query_strings to modes.
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "v", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+          -- if you set this to `true` (default is `false`) then any textobject is
+          -- extended to include preceding or succeeding whitespace. succeeding
+          -- whitespace has priority in order to act similarly to eg the built-in
+          -- `ap`.
+          --
+          -- can also be a function which gets passed a table with the keys
+          -- * query_string: eg '@function.inner'
+          -- * selection_mode: eg 'v'
+          -- and should return true of false
+          include_surrounding_whitespace = true,
+        },
         move = {
           enable = true,
           goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-          goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+          goto_nextf_end = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
           goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-          goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+          goto_previous_end = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
         },
       },
     })
