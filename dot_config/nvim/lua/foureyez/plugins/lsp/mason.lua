428271750071692089a9a5e4a4ec73a4b6b593bf
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-04-27 19:43:21 +0530

    fic

--- dot_config/nvim/lua/foureyez/plugins/lsp/mason.lua
+++ dot_config/nvim/lua/foureyez/plugins/lsp/mason.lua
@@ -1,25 +1,46 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    require("mason").setup({
-      ensure_installed = {
-        "gopls",
-        "goimports",
-        "gofumpt",
-        "gomodifytags",
-        "impl",
-        "delve",
-        "pyright",
-        "lua-language-server",
-        "bash-language-server",
-        "ruff",
-        "rust-analyzer",
-        "svelte-language-server",
-        "yaml-language-server",
-      },
-    })
-  end,
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+
+		local mason = require("mason")
+    mason.setup({})
+
+		local mason_tool_installer = require("mason-tool-installer")
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"prettier",
+				"prettierd",
+				"gofumpt",
+				"rustfmt",
+				"goimports-reviser",
+				"gomodifytags",
+				"stylua", --lua formatter
+				"impl", --golang generate method stubs for implementing interface
+				"black", --python formatter
+				"golangci-lint",
+				"pyright", --python linter
+				"ruff", --python linter and code formatter written in rust
+			},
+		})
+		local mason_lspconfig = require("mason-lspconfig")
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"gopls",
+				"lua-language-server",
+				"ansible-language-server",
+				"bash-language-server",
+				"rust-analyzer",
+				"svelte-language-server",
+				"yaml-language-server",
+				"helm-ls",
+
+				"delve",
+			},
+			automatic_installation = true,
+		})
+	end,
 }
