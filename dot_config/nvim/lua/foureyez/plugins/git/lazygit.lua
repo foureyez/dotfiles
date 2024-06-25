cda9d1735c929dde32bc361e7d0ad6f28a19382f
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-06-25 12:58:43 +0530

    add support for lazy config

--- dot_config/nvim/lua/foureyez/plugins/git/lazygit.lua
+++ dot_config/nvim/lua/foureyez/plugins/git/lazygit.lua
@@ -5,6 +5,9 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
+		vim.g.lazygit_use_custom_config_file_path = 1
+		vim.g.lazygit_config_file_path = vim.fn.expand("~/.config/nvim/lazy.yaml")
+
 		-- Add Mappings
 		local wk = require("which-key")
 		wk.register({
