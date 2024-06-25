ac3ba80676e364817416b37bbb35b5ba5363730b
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-06-25 13:14:33 +0530

    add semantic token

--- dot_config/nvim/lua/foureyez/plugins/lsp/lspconfig.lua
+++ dot_config/nvim/lua/foureyez/plugins/lsp/lspconfig.lua
@@ -63,6 +63,15 @@ return {
 
 			opts.desc = "Restart LSP"
 			vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+
+			if client.name == "gopls" and not client.server_capabilities.semanticTokensProvider then
+				local semantic = client.config.capabilities.textDocument.semanticTokens
+				client.server_capabilities.semanticTokensProvider = {
+					full = true,
+					legend = { tokenModifiers = semantic.tokenModifiers, tokenTypes = semantic.tokenTypes },
+					range = true,
+				}
+			end
 		end
 
 		-- Configure LSP for rust
