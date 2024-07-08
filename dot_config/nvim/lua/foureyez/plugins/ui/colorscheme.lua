ad5a88ebfd34f9a1e8706d33917fd6bf8e294a49
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-07-08 13:19:23 +0530

    Add colorschemes

--- dot_config/nvim/lua/foureyez/plugins/ui/colorscheme.lua
+++ dot_config/nvim/lua/foureyez/plugins/ui/colorscheme.lua
@@ -1,49 +1,30 @@
--- return {
--- 	"navarasu/onedark.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("onedark").setup({
--- 			style = "darker",
--- 		})
--- 		require("onedark").load()
--- 	end,
--- }
--- return {
--- 	"scottmckendry/cyberdream.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("cyberdream").setup({
--- 			-- Recommended - see "Configuring" below for more config options
--- 			transparent = true,
--- 			italic_comments = true,
--- 			hide_fillchars = true,
--- 			borderless_telescope = true,
--- 			terminal_colors = true,
--- 		})
--- 		vim.cmd("colorscheme cyberdream") -- set the colorscheme
--- 	end,
--- }
--- return {
--- 	"folke/tokyonight.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = {},
--- 	config = function()
--- 		vim.cmd("colorscheme tokyonight-moon")
--- 	end,
--- }
 return {
-  "rebelot/kanagawa.nvim",
-  lazy = false,
-  priority = 1000,
-  opts = {},
-  config = function()
-    vim.cmd("colorscheme kanagawa")
-  end,
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("cyberdream").setup({
+				transparent = true,
+				italic_comments = true,
+				hide_fillchars = true,
+				borderless_telescope = true,
+				terminal_colors = true,
+			})
+			-- vim.cmd("colorscheme cyberdream") -- set the colorscheme
+		end,
+	},
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function()
+			vim.cmd("colorscheme kanagawa")
+		end,
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
 }
--- return {
---   "rose-pine/neovim",
---   name = "rose-pine",
--- }
