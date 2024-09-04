adb9eb4789694f3fc27c5452e09116a9f31e8f91
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-09-04 15:35:47 +0530

    file browsing

---
+++ dot_config/nvim/lua/foureyez/plugins/utils/file-browsing.lua
@@ -0,0 +1,13 @@
+return {
+	{ "echasnovski/mini.files", version = false },
+	{
+		"stevearc/oil.nvim",
+		opts = {
+			view_options = {
+				show_hidden = true,
+			},
+		},
+		-- Optional dependencies
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
+}
