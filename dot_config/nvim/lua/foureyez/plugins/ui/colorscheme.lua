c186a750d91439fa8e01cf4e0b059169ab983769
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-09-29 00:29:32 +0530

    fix

--- dot_config/nvim/lua/foureyez/plugins/ui/colorscheme.lua
+++ dot_config/nvim/lua/foureyez/plugins/ui/colorscheme.lua
@@ -8,10 +8,25 @@ return {
 		priority = 1000,
 		config = function()
 			require("kanagawa").setup({
+				dimInactive = true,
 				compile = true, -- enable compiling the colorscheme
 				overrides = function(colors) -- add/modify highlights
 					local theme = colors.theme
 					local overrides = {
+						NormalFloat = { bg = "none" },
+						FloatBorder = { bg = "none" },
+						FloatTitle = { bg = "none" },
+
+						-- Save an hlgroup with dark background and dimmed foreground
+						-- so that you can use it where your still want darker windows.
+						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+						-- Popular plugins that open floats will link to NormalFloat by default;
+						-- set their background accordingly if you wish to keep them dark and borderless
+						LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+						MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
 						TelescopeTitle = { fg = theme.ui.special, bold = true },
 						TelescopePromptNormal = { bg = theme.ui.bg_p1 },
 						TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
