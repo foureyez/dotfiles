8dc651dde4c337cfa34d1b6ffd1644341507cb80
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-10-09 22:44:42 +0530

    remove nvimcmp and use blink

--- dot_config/nvim/lua/foureyez/plugins/coding/lsp/lspconfig.lua
+++ dot_config/nvim/lua/foureyez/plugins/coding/lsp/lspconfig.lua
@@ -2,7 +2,7 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
+		-- "hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 	},
 	opts = {
@@ -17,8 +17,9 @@ return {
 		},
 	},
 	config = function()
-		local cmp_nvim_lsp = require("cmp_nvim_lsp")
-		local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		-- local capabilities = cmp_nvim_lsp.default_capabilities()
+		local capabilities = {}
 		local lspconfig = require("lspconfig")
 
 		local opts = { noremap = true, silent = true }
--- dot_config/nvim/lua/foureyez/plugins/notetaking/obsidian.lua
+++ dot_config/nvim/lua/foureyez/plugins/notetaking/obsidian.lua
@@ -40,7 +40,7 @@ return {
 			-- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
 			completion = {
 				-- Set to false to disable completion.
-				nvim_cmp = true,
+				-- nvim_cmp = true,
 				-- Trigger completion at 2 chars.
 				min_chars = 2,
 			},
