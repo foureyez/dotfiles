8f7da81476835532c0b84175993e9ed8dcbebf67
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-10-11 17:20:57 +0530

    remove signature

--- dot_config/nvim/lua/foureyez/plugins/coding/plugins.lua
+++ dot_config/nvim/lua/foureyez/plugins/coding/plugins.lua
@@ -1,18 +1,5 @@
 return {
 	{
-		"ray-x/lsp_signature.nvim",
-		event = "InsertEnter",
-		opts = {
-			bind = true,
-			handler_opts = {
-				border = "none",
-			},
-		},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
-		end,
-	},
-	{
 		"windwp/nvim-ts-autotag",
 		config = function()
 			require("nvim-ts-autotag").setup({})
