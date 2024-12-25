d60aaf763e55fe696099874d6f8f1a6822f76431
Abhijeet Rawat <abhirawat1304@gmail.com>
2024-12-25 10:33:14 +0530

    add nvimtree back

--- dot_config/nvim/lua/foureyez/keymaps.lua
+++ dot_config/nvim/lua/foureyez/keymaps.lua
@@ -3,8 +3,8 @@ local map = vim.keymap
 
 local terminal = require("toggleterm.terminal").Terminal
 local float_term_opts = {
-	direction = "float",
-	hidden = true,
+  direction = "float",
+  hidden = true,
 }
 
 -- General keymaps
@@ -13,21 +13,22 @@ map.set("n", "<C-j>", ":FzfLua files<cr>", defaults)
 map.set("n", "<leader>tt", ":FzfLua files<cr>", defaults)
 map.set("n", "<C-k>", "kkkkk", defaults)
 map.set("n", "<leader>n", function()
-	vim.o.rnu = not vim.o.rnu
+  vim.o.rnu = not vim.o.rnu
 end, defaults)
 
 -- Terminal keymaps
 map.set("n", "<leader>l", function()
-	terminal:new(vim.tbl_deep_extend("force", float_term_opts, { cmd = "lazygit", dir = vim.fn.getcwd() })):toggle()
+  terminal:new(vim.tbl_deep_extend("force", float_term_opts, { cmd = "lazygit", dir = vim.fn.getcwd() })):toggle()
 end, defaults)
 map.set("n", "<leader>g", function()
-	terminal:new(vim.tbl_deep_extend("force", float_term_opts, { cmd = "gitui", dir = vim.fn.getcwd() })):toggle()
+  terminal:new(vim.tbl_deep_extend("force", float_term_opts, { cmd = "gitui", dir = vim.fn.getcwd() })):toggle()
 end, defaults)
 map.set("n", "<leader>tf", "<cmd>ToggleTerm direction=float<CR>", defaults)
 
 -- Mgmt Keymaps
 map.set("n", "<leader>;", "<cmd>NeovimProjectDiscover<CR>", defaults)
-map.set("n", "<leader>e", "<cmd>Oil --float<CR>", defaults)
+map.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", defaults)
+map.set("n", "<leader>w", "<cmd>Oil --float<CR>", defaults)
 
 map.set("n", "<leader>bl", "<cmd>FzfLua dap_breakpoints<CR>", defaults)
 map.set("n", "<leader>/", "<cmd>FzfLua live_grep<CR>", defaults)
@@ -38,16 +39,16 @@ map.set("n", "<leader>dl", "<cmd>FzfLua diagnostics_workspace<CR>", defaults)
 map.set("n", "<leader>b", "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>", defaults)
 map.set("n", "<leader>bc", "<cmd>lua require('persistent-breakpoints.api').clear_all_breakpoints()<cr>", defaults)
 map.set("n", "F3", function()
-	require("dap").step_over()
+  require("dap").step_over()
 end, defaults)
 map.set("n", "F2", function()
-	require("dap").step_into()
+  require("dap").step_into()
 end, defaults)
 map.set("n", "F4", function()
-	require("dap").step_out()
+  require("dap").step_out()
 end, defaults)
 map.set("n", "F5", function()
-	require("dap").continue()
+  require("dap").continue()
 end, defaults)
 
 -- Git Keymaps
