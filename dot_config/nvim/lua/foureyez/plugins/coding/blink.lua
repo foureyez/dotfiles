7e24bbb9dd445df0ec64e206b70aa272a2340583
Abhijeet Rawat <abhirawat1304@gmail.com>
2025-05-02 23:18:56 +0530

    update blink and disable linting

--- dot_config/nvim/lua/foureyez/plugins/coding/blink.lua
+++ dot_config/nvim/lua/foureyez/plugins/coding/blink.lua
@@ -5,7 +5,7 @@ return {
 			"rafamadriz/friendly-snippets",
 		},
 		-- use a release tag to download pre-built binaries
-		version = "v0.*",
+		version = "1.*",
 		-- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
 		-- build = 'cargo build --release',
 
@@ -13,13 +13,6 @@ return {
 			keymap = { preset = "super-tab" },
 
 			appearance = {
-				-- Sets the fallback highlight groups to nvim-cmp's highlight groups
-				-- Useful for when your theme doesn't support blink.cmp
-				-- will be removed in a future release
-				-- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-				use_nvim_cmp_as_default = true,
-
-				-- Adjusts spacing to ensure icons are aligned
 				nerd_font_variant = "mono",
 			},
 
@@ -38,17 +31,19 @@ return {
 			},
 
 			completion = {
+				documentation = { auto_show = false },
 				ghost_text = {
 					enabled = true,
 					show_with_menu = true,
 				},
 			},
 			cmdline = {
-				enabled = true,
+				keymap = {
+					preset = "inherit",
+				},
 				completion = {
-					menu = {
-						auto_show = true,
-					},
+					-- ghost_text = { enabled = true },
+					menu = { auto_show = true },
 				},
 			},
 		},
