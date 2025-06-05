d38e710e9f3a065085d4ad48d1a46688a7fd19fc
Abhijeet Rawat <abhirawat1304@gmail.com>
2025-06-05 10:00:44 +0530

    fix mason

--- dot_config/nvim/lua/foureyez/plugins/coding/mason.lua
+++ dot_config/nvim/lua/foureyez/plugins/coding/mason.lua
@@ -1,61 +1,11 @@
 return {
-  {
-    "mason-org/mason.nvim",
-    dependencies = {
-      "mason-org/mason-lspconfig.nvim",
-    },
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local mason = require("mason")
-      mason.setup()
-
-      local mason_lspconfig = require("mason-lspconfig")
-      mason_lspconfig.setup({
-        ensure_installed = {
-          -- "gopls",
-          -- "lua_ls",        -- lua-language-server
-          -- "ansible-language-server",
-          -- "ts_ls",         -- typescript-language-server
-          -- "tailwindcss", -- tailwindcss-language-server
-          "bashls",        -- bash-langugage-server
-          "rust_analyzer", -- rust-analyzer
-          -- "svelte", -- svelte-language-server
-          -- "yaml-language-server",
-          "helm_ls",
-          -- "ols",
-          "glsl_analyzer",
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    dependencies = {
-      "mason-org/mason.nvim",
-    },
-    lazy = true,
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-      local mason_tool_installer = require("mason-tool-installer")
-      mason_tool_installer.setup({
-        ensure_installed = {
-          "djlint",
-          "prettierd",
-          "gofumpt",
-          "rustfmt",
-          "goimports-reviser",
-          "gomodifytags",
-          "stylua", --lua formatter
-          "impl",   --golang generate method stubs for implementing interface
-          "black",  --python formatter
-          "golangci-lint",
-          -- "pyright", --python linter
-          -- "ruff", --python linter and code formatter written in rust
-          "delve",
-        },
-      })
-    end,
-  },
+	{
+		"mason-org/mason.nvim",
+		lazy = true,
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local mason = require("mason")
+			mason.setup()
+		end,
+	},
 }
