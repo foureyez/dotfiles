9f40b7f46789eefcd46f6dca144c3038189812a4
Abhijeet Rawat <abhirawat1304@gmail.com>
2025-06-08 10:54:38 +0530

    fix

--- dot_config/nvim/lua/foureyez/plugins/ui/colorscheme.lua
+++ dot_config/nvim/lua/foureyez/plugins/ui/colorscheme.lua
@@ -86,42 +86,42 @@ return {
 			})
 		end,
 	},
-	-- {
-	-- 	"rebelot/kanagawa.nvim",
-	-- 	lazy = false,
-	-- 	priority = 1000,
-	-- 	config = function()
-	-- 		require("kanagawa").setup({
-	-- 			-- dimInactive = true,
-	-- 			compile = true, -- enable compiling the colorscheme
-	-- 			overrides = function(colors) -- add/modify highlights
-	-- 				local theme = colors.theme
-	-- 				local overrides = {
-	-- 					NormalFloat = { bg = "none" },
-	-- 					FloatBorder = { bg = "none" },
-	-- 					FloatTitle = { bg = "none" },
-	--
-	-- 					-- Save an hlgroup with dark background and dimmed foreground
-	-- 					-- so that you can use it where your still want darker windows.
-	-- 					-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-	-- 					NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-	--
-	-- 					-- Popular plugins that open floats will link to NormalFloat by default;
-	-- 					-- set their background accordingly if you wish to keep them dark and borderless
-	-- 					LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-	-- 					MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-	--
-	-- 					Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
-	-- 					PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
-	-- 					PmenuSbar = { bg = theme.ui.bg_m1 },
-	-- 					PmenuThumb = { bg = theme.ui.bg_p2 },
-	--
-	-- 					NvimTreeWinSeparator = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-	-- 				}
-	-- 				return overrides
-	-- 			end,
-	-- 			theme = "wave", -- Load "wave" theme when 'background' option is not set
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("kanagawa").setup({
+				-- dimInactive = true,
+				compile = true, -- enable compiling the colorscheme
+				overrides = function(colors) -- add/modify highlights
+					local theme = colors.theme
+					local overrides = {
+						NormalFloat = { bg = "none" },
+						FloatBorder = { bg = "none" },
+						FloatTitle = { bg = "none" },
+
+						-- Save an hlgroup with dark background and dimmed foreground
+						-- so that you can use it where your still want darker windows.
+						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+
+						-- Popular plugins that open floats will link to NormalFloat by default;
+						-- set their background accordingly if you wish to keep them dark and borderless
+						LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+						MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+
+						Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparency
+						PmenuSel = { fg = "NONE", bg = theme.ui.bg_p2 },
+						PmenuSbar = { bg = theme.ui.bg_m1 },
+						PmenuThumb = { bg = theme.ui.bg_p2 },
+
+						NvimTreeWinSeparator = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+					}
+					return overrides
+				end,
+				theme = "wave", -- Load "wave" theme when 'background' option is not set
+			})
+		end,
+	},
 }
