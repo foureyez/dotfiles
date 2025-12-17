c2b28bbef5f4a64f8b0a34a916fef4271496b549
Abhijeet Rawat <abhirawat1304@gmail.com>
2025-12-17 12:17:29 +0530

    update ts

--- dot_config/nvim/lua/foureyez/plugins/coding/treesitter.lua
+++ dot_config/nvim/lua/foureyez/plugins/coding/treesitter.lua
@@ -1,11 +1,12 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	event = { "BufReadPre", "BufNewFile" },
+  lazy = false,
 	dependencies = {},
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
+		local ts = require("nvim-treesitter")
+    ts.install(
+			 {
 				"markdown",
 				"lua",
 				"go",
@@ -23,29 +24,7 @@ return {
 				"rust",
 				"odin",
 				"glsl",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				language_tree = true,
-				additional_vim_regex_highlighting = false,
-				-- disable = function()
-				-- 	return vim.b.large_buf
-				-- end,
-			},
-			indent = {
-				enable = true,
-				disable = { "yaml", "python" },
-			},
-			-- incremental_selection = {
-			-- 	enable = true,
-			-- 	keymaps = {
-			-- 		init_selection = "<C-v>",
-			-- 		node_incremental = "<C-v>",
-			-- 		node_decremental = "<C-V>",
-			-- 		scope_incremental = false,
-			-- 	},
-			-- },
-		})
+			}
+    )
 	end,
 }
