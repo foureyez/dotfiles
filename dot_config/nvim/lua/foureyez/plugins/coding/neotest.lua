87610440e46200b07190bd8e67a13788158682bd
Abhijeet Rawat <abhirawat1304@gmail.com>
2026-01-21 14:42:18 +0530

    fix neotest

--- dot_config/nvim/lua/foureyez/plugins/coding/neotest.lua
+++ dot_config/nvim/lua/foureyez/plugins/coding/neotest.lua
@@ -1,18 +1,32 @@
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"antoinemadec/FixCursorHold.nvim",
-		{ "fredrikaverpil/neotest-golang", version = "*" },
-	},
-	lazy = true,
-	cmd = "Neotest run",
-	config = function()
-		require("neotest").setup({
-			adapters = {
-				require("neotest-golang"), -- Registration
-			},
-		})
-	end,
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    {
+      "nvim-treesitter/nvim-treesitter", -- Optional, but recommended
+      branch = "main",                   -- NOTE; not the master branch!
+      build = function()
+        vim.cmd(":TSUpdate go")
+      end,
+    },
+    {
+      "fredrikaverpil/neotest-golang",
+      version = "*",
+      build = function()
+        vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+      end,
+    },
+  },
+  config = function()
+    local config = {
+      runner = "gotestsum",
+    }
+    require("neotest").setup({
+      adapters = {
+        require("neotest-golang")(config),
+      },
+    })
+  end,
 }
