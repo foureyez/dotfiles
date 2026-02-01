3c55409400b96d3d234a3c724c790c574bdce923
Abhijeet Rawat <abhirawat1304@gmail.com>
2026-02-01 13:44:08 +0530

    fix ghostty config

--- dot_config/nvim/lua/foureyez/plugins/coding/neotest.lua
+++ dot_config/nvim/lua/foureyez/plugins/coding/neotest.lua
@@ -1,32 +1,34 @@
 return {
-  "nvim-neotest/neotest",
-  dependencies = {
-    "nvim-neotest/nvim-nio",
-    "nvim-lua/plenary.nvim",
-    "antoinemadec/FixCursorHold.nvim",
-    {
-      "nvim-treesitter/nvim-treesitter", -- Optional, but recommended
-      branch = "main",                   -- NOTE; not the master branch!
-      build = function()
-        vim.cmd(":TSUpdate go")
-      end,
-    },
-    {
-      "fredrikaverpil/neotest-golang",
-      version = "*",
-      build = function()
-        vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
-      end,
-    },
-  },
-  config = function()
-    local config = {
-      runner = "gotestsum",
-    }
-    require("neotest").setup({
-      adapters = {
-        require("neotest-golang")(config),
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      {
+        "nvim-treesitter/nvim-treesitter", -- Optional, but recommended
+        branch = "main",                   -- NOTE; not the master branch!
+        build = function()
+          vim.cmd(":TSUpdate go")
+        end,
       },
-    })
-  end,
+      {
+        "fredrikaverpil/neotest-golang",
+        version = "*",                                                            -- Optional, but recommended; track releases
+        build = function()
+          vim.system({ "go", "install", "gotest.tools/gotestsum@latest" }):wait() -- Optional, but recommended
+        end,
+      },
+    },
+    config = function()
+      local config = {
+        runner = "gotestsum", -- Optional, but recommended
+      }
+      require("neotest").setup({
+        adapters = {
+          require("neotest-golang")(config),
+        },
+      })
+    end,
+  },
 }
